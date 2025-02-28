8fc0e1ab213e0ad753756e977a5e24fd041c4266
Madhu <madhumudhiraj77@gmail.com>
2025-02-28 11:05:53 +0530

    Updated API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
-const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
-export const SWIGGY_APID  = PROXY_URL + "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.387158&lng=78.545031&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const SWIGGY_API  =  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.387158&lng=78.545031&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-export const SWIGGY_API = PROXY_URL + "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=17.387158&lng=78.545031&carousel=true&third_party_vendor=1";
+export const SWIGGY_APIM =  "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=17.387158&lng=78.545031&carousel=true&third_party_vendor=1";
 export const SWIGGY_CATEGORY_API = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"
 export const RESTAURANT_LOGO = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 // https://www.swiggy.com
